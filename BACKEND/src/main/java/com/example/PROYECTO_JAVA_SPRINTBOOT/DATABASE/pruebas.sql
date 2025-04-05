e6a133ab0882293e1eecce837ea4b2b2b3fb70a8
Grabiel1212 <juangrabielbarbozarivera12@gmial.com>
2025-04-05 14:21:30 -0500

    subiendo sql ala nube

--- BACKEND/src/main/java/com/example/PROYECTO_JAVA_SPRINTBOOT/DATABASE/pruebas.sql
+++ BACKEND/src/main/java/com/example/PROYECTO_JAVA_SPRINTBOOT/DATABASE/pruebas.sql
@@ -7,3 +7,12 @@ SELECT id_cancion, titulo, pista_subtitulo, imagen FROM canciones WHERE id_canci
 
 SELECT * FROM playlists;
 SELECT * FROM playlist_canciones;
+
+SELECT u.id_usuario ,pl.id_playlist , u.nombre as Usuario  , pl.nombre as playlist   ,c.titulo  as cancion , al.titulo as albun , ar.nombre as artista FROM playlists  as pl
+INNER JOIN playlist_canciones AS pc on pl.id_playlist = pc.id_playlist
+INNER JOIN usuarios as u ON  u.id_usuario = pl.id_usuario
+INNER JOIN canciones as c ON c.id_cancion = pc.id_cancion
+INNER JOIN albumes as al ON al.id_album = c.id_album
+INNER JOIN artistas as ar on ar.id_artista = c.id_artista
+WHERE u.id_usuario = 1 or pl.id_playlist = 1;
+;
